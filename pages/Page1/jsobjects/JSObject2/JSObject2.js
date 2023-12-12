a19bbb5d816cb3d678eb5a9e9075ce7f96b5da3f
Mahesh babu <mahesh.babu@drivex.in>
2023-12-12 08:38:07 +0000

    jo

--- pages/Page1/jsobjects/JSObject2/JSObject2.js
+++ pages/Page1/jsobjects/JSObject2/JSObject2.js
@@ -1,12 +1,64 @@
 export default {
-	myVar1: [],
-	myVar2: {},
-	myFun1 () {
-		//	write code here
-		//	this.myVar1 = [1,2,3]
-	},
-	async myFun2 () {
-		//	use async-await or promises
-		//	await storeValue('varName', 'hello world')
-	}
+	fetchData() {
+  const inputData =  Query2.data
+
+  return inputData;
+},
+
+option :{
+  title: {
+    text: 'procure'
+  },
+  tooltip: {
+    trigger: 'item',
+  formatter: '{b}: {c}%'
+
+  },
+  toolbox: {
+    feature: {
+      dataView: { readOnly: false },
+      restore: {},
+      saveAsImage: {}
+    }
+  },
+ 
+  series: [
+    {
+      name: 'Funnel',
+      type: 'funnel',
+      left: '8%',
+      top: 60,
+      bottom: 30,
+      width: '80%',
+      min: 0,
+      max: 100,
+      minSize: '0%',
+      maxSize: '100%',
+      sort: 'descending',
+      gap: 2,
+     label: {
+          show: true,
+          position: 'inside',
+          formatter: '{b}: {c}%'
+		 },
+      labelLine: {
+        length: 10,
+        lineStyle: {
+          width: 1,
+          type: 'solid'
+        }
+      },
+      itemStyle: {
+        borderColor: '#fff',
+        borderWidth: 1
+      },
+      emphasis: {
+        label: {
+          fontSize: 20
+        }
+      },
+      data: this.fetchData()
+    }
+  ]
+}
 }
